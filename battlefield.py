d03a504f6e400e9e450439d5017397afd0b76e3e
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-01-05 23:54:40 -0500

    Edited a few more comments to make things clearer.

--- battlefield.py
+++ battlefield.py
@@ -27,20 +27,22 @@ class Battlefield:
         The first wave is a Tyrannosaurus Rex!
         ''')
 
-    # This function throws the names of the combatant objects into a list of strings.
+    # This function throws the names of the combatant objects into lists of strings.
     def list_names_of_combatants(self):
         self.herd_names = [] 
         for dinosaur in self.angry_herd.herd:
             if len(self.herd_names) < 3:
                 self.herd_names.append(dinosaur.name)
-        # print(self.herd_names)
+       
 
         self.fleet_names = [] 
         for robot_soldier in self.readied_fleet.fleet:
             if len(self.fleet_names) < 3:
                 self.fleet_names.append(robot_soldier.name)
-        # print(self.fleet_names)
-    
+        
+    # This is all the fighting logic. Robots and dinosaurs alternate turns and have a chance of hitting or missing
+    # during an attack sequence. The user is expected to hit 'Enter' before each turn is taken so that the user can
+    # follow the action.
     def battle(self):
         battle_counter = 0
         attack_attempt = False
@@ -77,15 +79,9 @@ class Battlefield:
                     battle_counter += 1
                     self.versus_battle_stats()
                     self.opponent_death()
-            # elif battle_counter % 2 == 0:
-            #     print(input('Command your fleet! Press Enter to attack: '))
-            #     self.readied_fleet.fleet[0].attack(self.angry_herd.herd[self.correct_dino_index])
-            #     battle_counter += 1
-            #     self.versus_battle_stats()
-            #     self.opponent_death()
      
      
-     # Makes sure the correct opponents health is being modified by that attacks in the battle.           
+     # Makes sure the correct opponents health is being modified by the attacks in the battle.           
     def correct_opponent_health(self):
         if len(self.fleet_names) == 3:
             self.correct_robot_index = 0
@@ -106,7 +102,7 @@ class Battlefield:
             pass
    
     # Checks to see if health is equal to zero or below and, if so, pops the name out of herd_names
-    # or fleet_names so that I can move onto the next opponent in the list via versus_battle_stats().
+    # or fleet_names so that I can move onto the next opponent in the list via versus_battle_stats() below.
     def opponent_death(self):
         self.loser = ''
         if self.dino_stats[1] <= 0:
@@ -174,32 +170,22 @@ class Battlefield:
                 self.show_robo_oppenent_options()
                 print(' ')
            
-    
+    # Appends all dinosaur attributes to a list.
     def dino_turn(self, dinosaur):
         self.dino_stats = []
         self.dino_stats.append(dinosaur.name)
         self.dino_stats.append(dinosaur.health)
         self.dino_stats.append(dinosaur.attack_power)
-        # print(self.dino_stats)
 
+    # Appends all robot object attributes to a list.
     def robo_turn(self, robot):
         self.robo_stats = []
         self.robo_stats.append(robot.name)
         self.robo_stats.append(robot.health)
         self.robo_stats.append(robot.weapon.name)
         self.robo_stats.append(robot.weapon.attack_power)
-        # print(self.robo_stats)
 
-        # for robot_soldier in self.readied_fleet.fleet:
-        #     if robot_soldier.name == fleet_names[random.randrange(0-2)]:
-        #         print(robot_soldier.name)
-        #         break
-        
-        # for robot_soldier in self.readied_fleet.fleet:
-        #     if robot_soldier.weapon.name == 'Sword':
-        #         print(robot_soldier.weapon.name)
-        #         break
-
+    # Index[0] will always be 'Name', index[1] Health, and index[2] attack power.
     def show_dino_opponent_options(self):
         display_stats = False
         index = 0
@@ -213,8 +199,8 @@ class Battlefield:
             elif index == 2:
                 print(f'Attack Power: {self.dino_stats[2]}')
                 break
-        # Index[0] will always be 'Name', index[1] Health, and index[2] attack power.
-
+        
+    # Index[0] will always be 'Name', index[1] Health, index[2] weapon, and index[3] attack power.
     def show_robo_oppenent_options(self):
         display_stats = False
         index = 0
@@ -231,8 +217,8 @@ class Battlefield:
             elif index == 3:
                 print(f'Attack Power: {self.robo_stats[3]}')
                 break
-        # Index[0] will always be 'Name', index[1] Health, index[2] weapon, and index[3] attack power.
 
+    # Prints the names of the surviving victors of the battle.
     def display_winners(self):
         if len(self.fleet_names) == 0:
             print('The victors are:')
